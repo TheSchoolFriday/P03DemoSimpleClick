b997868b10514423a2e01ef5bee4db80b5c372c0
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-07 11:51:12 +0800

    viewOnClickListener

--- app/src/main/java/sg/edu/rp/c346/id20046797/p03demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/p03demosimpleclick/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.security.KeyStore;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +34,17 @@ public class MainActivity extends AppCompatActivity {
 
                 tvDisplay.setText(strResponse);
             }
-        }
-        );
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tbtn.isChecked()) {
+                    etInput.setEnabled(true);
+                } else {
+                    etInput.setEnabled(false);
+                }
+            }
+        });
     }
 }
