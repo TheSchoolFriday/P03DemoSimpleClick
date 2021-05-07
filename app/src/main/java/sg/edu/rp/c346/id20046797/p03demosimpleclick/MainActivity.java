42db4df97dfdc8a5079f76cfade59fd9a083005b
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-07 12:11:44 +0800

    viewOnClickListener

--- app/src/main/java/sg/edu/rp/c346/id20046797/p03demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/p03demosimpleclick/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnDisplay;
     EditText etInput;
     ToggleButton tbtn;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,19 @@ public class MainActivity extends AppCompatActivity {
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
         tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioButtonGender);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String strResponse = etInput.getText().toString();
+                int checkRadioId = rgGender.getCheckedRadioButtonId();
 
+                if (checkRadioId == R.id.radioGroupGenderMale){
+                    strResponse = "He Says " + strResponse;
+                } else {
+                    strResponse = "She Says " + strResponse;
+                }
                 tvDisplay.setText(strResponse);
             }
         });
@@ -46,5 +56,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 }
